dc076180dd201884a1c1b462033a1c0a8ab15a4f
anth <zyle1104@gmail.com>
2024-08-02 11:03:51 +0900

    ♻️ Refactor: queue infra V1

--- src/infrastructure/queue/queue.repository.ts
+++ src/infrastructure/queue/queue.repository.ts
@@ -1,86 +1,39 @@
 import { Injectable } from '@nestjs/common';
-import { Repository } from '../base/base-repository';
-import { IQueueRepository } from 'src/domain/queue/i.queue.repository';
-import { QueueEntity } from './queue.entity';
-import { Queue, QueueStatusEnum } from 'src/domain/queue/queue';
-import { EntityManager, EntityTarget } from 'typeorm';
-import { QueueMapper } from './queue.mapper';
+import { Queue } from 'src/domain/queue/models/queue';
+import { IQueueRepository } from 'src/domain/queue/repositories/i.queue.repository';
+import { RedisQueueRepositoryImpl } from 'src/infrastructure/redis/redis-queue.repository';
 
 @Injectable()
-export class QueueRepositoryImpl
-  extends Repository<QueueEntity>
-  implements IQueueRepository
-{
-  protected entityClass: EntityTarget<QueueEntity> = QueueEntity;
+export class QueueRepositoryImpl implements IQueueRepository {
+  constructor(
+    private readonly redisQueueRepositoryImpl: RedisQueueRepositoryImpl,
+  ) {}
 
-  async save(
-    args: Queue,
-    transactionalEntityManager?: EntityManager,
-  ): Promise<Queue> {
-    const entity = new QueueEntity(args);
-    const excute = transactionalEntityManager
-      ? await transactionalEntityManager.save(entity)
-      : await this.getManager().save(entity);
-    return QueueMapper.toDomain(excute);
+  async registerWaitingQueue(userId): Promise<Queue | null> {
+    return await this.redisQueueRepositoryImpl.registerWaitingQueue(userId);
   }
 
-  async findByQueueId(args: { queueId: number }): Promise<Queue> {
-    const entity = await this.getManager().findOne(this.entityClass, {
-      where: { id: args.queueId },
-    });
-    return QueueMapper.toDomain(entity);
+  async findByUserIdWatingPosition(userId: number): Promise<number> {
+    return await this.redisQueueRepositoryImpl.findByUserIdWatingPosition(
+      userId,
+    );
   }
 
-  async findByUserId(args: { userId: number }): Promise<Queue> {
-    const entity = await this.getManager().findOne(this.entityClass, {
-      where: { userId: args.userId },
-    });
-    return QueueMapper.toDomain(entity);
+  async findByUserIdExistActiveToken(userId: number): Promise<number> {
+    return await this.redisQueueRepositoryImpl.findByUserIdExistActiveQueue(
+      userId,
+    );
   }
 
-  async findByQueueIdWaitingAhead(args: { queueId: number }): Promise<Queue> {
-    const entity = await this.getManager()
-      .createQueryBuilder(this.entityClass, 'queue')
-      .andWhere('queue.status = :status', { status: QueueStatusEnum.WAITING })
-      .andWhere('queue.id < :queueId', { queueId: args.queueId })
-      .orderBy('queue.createdAt', 'ASC')
-      .getOne();
-
-    return QueueMapper.toDomain(entity);
+  async moveToActiveToken(limit: number, ttl: number): Promise<void> {
+    return await this.redisQueueRepositoryImpl.moveToActiveQueue(limit, ttl);
   }
 
-  async findByQueueIdAndUserId(args: {
-    queueId: number;
-    userId: number;
-  }): Promise<Queue> {
-    const entity = await this.getManager().findOne(this.entityClass, {
-      where: { id: args.queueId, userId: args.userId },
-    });
-    return QueueMapper.toDomain(entity);
+  async getUserStatus(userId: number): Promise<string> {
+    return await this.redisQueueRepositoryImpl.getUserStatus(userId);
   }
 
-  async findExpiredActiveRecords(): Promise<Queue[]> {
-    const entities = await this.getManager()
-      .createQueryBuilder(this.entityClass, 'queue')
-      .where('queue.status = :status', { status: QueueStatusEnum.IN_PROGRESS })
-      .andWhere('queue.expiredAt < NOW()')
-      .getMany();
-    return entities.map((entity) => QueueMapper.toDomain(entity));
-  }
-  async findActiveRecordsCount(): Promise<number> {
-    return await this.getManager().count(this.entityClass, {
-      where: { status: QueueStatusEnum.IN_PROGRESS },
-    });
-  }
-  async findWaitingRecords(args: { limit: number }): Promise<Queue[]> {
-    const entities = await this.getManager().find(this.entityClass, {
-      where: { status: QueueStatusEnum.WAITING },
-      take: args.limit,
-    });
-    return entities.map((entity) => QueueMapper.toDomain(entity));
-  }
-  async saveAll(args: Queue[]): Promise<Queue[]> {
-    const entities = await this.getManager().save(this.entityClass, args);
-    return entities.map((entity) => QueueMapper.toDomain(entity));
+  async clearActiveToken(userId: number): Promise<void> {
+    return await this.redisQueueRepositoryImpl.clearActiveQueue(userId);
   }
 }
