e1c7f9b2607b8d154ad7015302acf97ab648fa8c
anth <zyle1104@gmail.com>
2024-08-23 03:32:49 +0900

    📝 Docs: stuff

---
+++ queue/jest.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  maxWorkers: 1,
+  moduleFileExtensions: ['js', 'json', 'ts'],
+  rootDir: '.',
+  testEnvironment: 'node',
+  testRegex: '.*\\.spec\\.ts$',
+  transform: {
+    '^.+\\.(t|j)s$': 'ts-jest',
+  },
+  moduleNameMapper: {
+    '^src/(.*)$': '<rootDir>/src/$1',
+  },
+  forceExit: true,
+};
